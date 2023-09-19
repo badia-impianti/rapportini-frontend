63945b886d46123e31a55bd29fd99529b1c6279f
leokx6 <leonardo.zaza1@gmail.com>
2023-09-19 16:39:23 +0200

    Primo commit

---
+++ src/Pages/Home.js
@@ -0,0 +1,125 @@
+import React from "react";
+import { useState } from "react";
+import { IoPerson, IoCalendar, IoClipboard, IoBulb, IoPeople } from "react-icons/io5";
+
+const Home = () => {
+
+    const reports = [
+        {
+            date: "01/01/2021",
+            customer: "Mario Rossi",
+            description: "La luce non funziona",
+            material: ["Lampadina", "Cavo"],
+            workers: ["Mario Bianchi", "Luigi Verdi"],
+            vehicles: ["Ducato", "Camioncino"],
+        },
+        {
+            date: "02/01/2021",
+            customer: "Mario Rossi",
+            description: "La luce non funziona",
+            material: ["Lampadina", "Cavo"],
+            workers: ["Mario Bianchi", "Luigi Verdi"],
+            vehicles: ["Ducato", "Camioncino"],
+        },
+        {
+            date: "03/01/2021",
+            customer: "Mario Rossi",
+            description: "La luce non funziona, quindi bisogna cambiare la lampadina e il cavo",
+            material: ["Lampadina", "Cavo"],
+            workers: ["Mario Bianchi", "Luigi Verdi"],
+            vehicles: ["Ducato", "Camioncino"],
+        },
+        {
+            date: "04/01/2021",
+            customer: "Mario Rossi",
+            description: "La luce non funziona",
+            material: ["Lampadina", "Cavo"],
+            workers: ["Mario Bianchi", "Luigi Verdi"],
+            vehicles: ["Ducato", "Camioncino"],
+        },
+        {
+            date: "05/01/2021",
+            customer: "Mario Rossi",
+            description: "La luce non funziona",
+            material: ["Lampadina", "Cavo"],
+            workers: ["Mario Bianchi", "Luigi Verdi"],
+            vehicles: ["Ducato", "Camioncino"],
+        },
+        {
+            date: "06/01/2021",
+            customer: "Mario Rossi",
+            description: "La luce non funziona",
+            material: ["Lampadina", "Cavo"],
+            workers: ["Mario Bianchi", "Luigi Verdi"],
+            vehicles: ["Ducato", "Camioncino"],
+        },
+        {
+            date: "07/01/2021",
+            customer: "Mario Rossi",
+            description: "La luce non funziona",
+            material: ["Lampadina", "Cavo"],
+            workers: ["Mario Bianchi", "Luigi Verdi"],
+            vehicles: ["Ducato", "Camioncino"],
+        },
+        {
+            date: "08/01/2021",
+            customer: "Mario Rossi",
+            description: "La luce non funziona",
+            material: ["Lampadina", "Cavo"],
+            workers: ["Mario Bianchi", "Luigi Verdi"],
+            vehicles: ["Ducato", "Camioncino"],
+        },
+        {
+            date: "09/01/2021",
+            customer: "Mario Rossi",
+            description: "La luce non funziona",
+            material: ["Lampadina", "Cavo"],
+            workers: ["Mario Bianchi", "Luigi Verdi"],
+            vehicles: ["Ducato", "Camioncino"],
+        }
+    ]
+
+    const [selectedReport, setSelectedReport] = useState(reports[0]);
+
+    return (
+        <div>
+        <h1>Home</h1>
+        <table>
+        <thead>
+                <tr>
+                    <th><IoCalendar size={20} style={{verticalAlign: "bottom", marginRight: "4px"}}/> Data</th>
+                    <th><IoPerson size={20} style={{verticalAlign: "bottom", marginRight: "4px"}}/> Cliente</th>
+                    <th><IoClipboard size={20} style={{verticalAlign: "bottom", marginRight: "4px"}}/> Descrizione</th>
+                    <th><IoPeople size={20} style={{verticalAlign: "bottom", marginRight: "4px"}}/> Operatori</th>
+                    <th><IoBulb size={20} style={{verticalAlign: "bottom", marginRight: "4px"}}/> Materiali</th>
+                    <th />
+                </tr>
+            </thead>
+            <tbody>
+                {reports.map((report) => (
+                    <tr>
+                        <td><p className="date">{report.date}</p></td>
+                        <td>{report.customer}</td>
+                        <td>{report.description}</td>
+                        <td>{report.workers.map
+                            ((worker) => (
+                                <p className="table_elements">{worker}</p>
+                            ))}
+                        </td>
+                        <td>{report.material.map
+                            ((material) => (
+                                <p className="table_elements">{material}</p>
+                            ))}
+                        </td>
+                        <td>
+                            <button className="button">Espandi</button>
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+        </div>
+    );
+    }
+
+export default Home;
---
+++ src/Pages/Login.js
@@ -0,0 +1,119 @@
+import React from "react";
+import { Router } from "react-router-dom";
+import Electricity from "../Images/Electricity.jpg";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+
+const Login = () => {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const navigate = useNavigate();
+
+    const login = (e) => {
+        e.preventDefault();
+        console.log("Login successful");
+        fetch("http://localhost:3001/login", {
+            method: "POST",
+            headers: {
+            "Content-Type": "application/json",
+            },
+            credentials: "include",
+            body: JSON.stringify({
+            email,
+            password,
+            }),
+        })
+            .then((res) => res.json())
+            .then((data) => {
+            console.log(data);
+            navigate("/home");
+            });
+    }
+
+
+    return (
+        (window.innerWidth > 1000) ?
+        <div style={{ display: "flex", flexDirection: "row", width: "100%", height: window.innerHeight + 1}}>
+            <div style={{ position: "absolute", height: window.innerHeight + 1, width: 100, right: window.innerWidth / 100 * 30 - 50, borderRadius: "50%", backgroundColor: "#ffffff"}}/>
+            <div style={{ width: "70%", backgroundImage: `url(${Electricity})`, backgroundSize: "cover", backgroundPosition: "center" }}/>
+            <div style={{ width: "30%", margin: "auto" }}>
+                <div style={{ width: "85%", margin: "auto", backgroundColor: "white", padding: "20px", display: "flex", flexDirection: "column", alignItems: "center" }}>
+                    <h1 style={{fontWeight: "bold", color: "#257cff", fontSize: 40, marginBottom: 40}}>Bentornato!</h1>
+                    <form onSubmit={login} >
+                        <div class="form__group field" >
+                            <input
+                                type="text"
+                                id="email"
+                                className="form__field"
+                                placeholder="Email"
+                                autoComplete="off"
+                                required
+                                text={email}
+                                onChange={e => setEmail(e.target.value)}
+                            />
+                            <label for="email" class="form__label">Email</label>
+                        </div>
+                        <div class="form__group field">
+                            <input
+                                type="password"
+                                id="password"
+                                className="form__field"
+                                placeholder="Password"
+                                required
+                                text={password}
+                                onChange={e => setPassword(e.target.value)}
+                            />
+                            <label for="password" class="form__label">Password</label>
+                        </div>
+                        <input type="submit" value="Login" className="button" style={{marginTop: 35}} />
+                    </form>
+                </div>
+                <p style={{color: "grey", position: "absolute", bottom: 10, right: 10}} > {new Date().toLocaleString()} </p>
+            </div>
+        </div>
+        :
+        <div style={{ display: "flex", flexDirection: "column", width: "100%", height: window.innerHeight + 1}}>
+            <div style={{ width: "100%", margin: "auto" }}>
+                <div style={{ width: "90%", margin: 0, backgroundColor: "white", padding: "20px", display: "flex", flexDirection: "column", alignItems: "center" }}>
+                    <h1 style={{fontWeight: "bold", color: "#257cff", fontSize: 40, marginBottom: 40}}>Bentornato!</h1>
+                    <form onSubmit={login}>
+                        <div class="form__group field" >
+                            <input
+                                type="text"
+                                id="email"
+                                className="form__field"
+                                placeholder="Email"
+                                autoComplete="off"
+                                required
+                                text={email}
+                                onChange={e => setEmail(e.target.value)}
+                            />
+                            <label for="email" class="form__label">Email</label>
+                        </div>
+                        <div class="form__group field">
+                            <input
+                                type="password"
+                                id="password"
+                                className="form__field"
+                                placeholder="Password"
+                                required
+                                text={password}
+                                onChange={e => setPassword(e.target.value)}
+                            />
+                            <label for="password" class="form__label">Password</label>
+                        </div>
+                        <input type="submit" value="Login" className="button" style={{marginTop: 35}} />
+                    </form>
+                </div>
+                <p style={{color: "grey", position: "absolute", bottom: 10, right: 10}} > {new Date().toLocaleString()} </p>
+            </div>
+        </div>
+        
+
+    );
+}
+
+export default Login;

081e19c8b9216097835b43cd3ab818a1f6221bce
mattiarainieri <mattiarainieri@live.it>
2023-09-28 22:03:34 +0200

    - Prima bozza di pagina specifica per un lavoro

--- src/Pages/Work.js
+++ src/Pages/Work.js
@@ -2,6 +2,7 @@ import {useEffect, useState} from "react";
 import {useParams} from "react-router-dom";
 import LoadingSpinner from "../components/LoadingSpinner";
 import LoadingError from "../components/LoadingError";
+import {IoPerson, IoTime, IoBookmark} from "react-icons/io5";
 
 import "./Work.css"
 
@@ -12,6 +13,7 @@ const Work = () => {
     //Page state
     const [isLoading, setIsLoading] = useState(true);
     const [loadingError, setLoadingError] = useState(false)
+    const [imagesUrls, setImagesUrls] = useState([{url:"", icon:""}]); //Array of strings [url1, url2, ...
 
     //Page data
     const [work, setWork] = useState({});
@@ -39,18 +41,87 @@ const Work = () => {
             });
     }, []);
 
+    useEffect(() => {
+        fetch("https://backend.rapportini.rainierihomecollection.it/works/" + id + "/images", {
+            method: "GET",
+            credentials: "include",
+        })
+            .then((res) => {
+                if (res.status === 200) {
+                    res.json().then((data) => {
+                        console.log(imagesUrls)
+                        setImagesUrls(data.images);
+                        console.log(data)
+                    });
+                }
+                else if (res.status === 404) {
+                    setLoadingError(true)
+                }
+            })
+    }, []);
+
     return (
-        loadingError ? <LoadingError errorDescription={"Il rapporto è stato cancellato o non è stato trovato"} /> :
+        loadingError ? <LoadingError/> :
         isLoading ? <LoadingSpinner /> :
             <div className="mainContainer">
                 <h1>Riepilogo per il lavoro n°{work.id}</h1>
-                <h2>Ecco di seguito alcune informazioni generali</h2>
-                <ul>
-                    <li>
-                        description: {work.description}
-                    </li>
+                <div className="infoAndPhotosContainer">
+                    <div className="infoContainer">
+                        <p><IoPerson /> <b>{ work.customer } </b></p>
+                        <p><IoTime/> <b>Iniziato:</b> { work.startTime } -  <IoTime /> <b>Terminato:</b> { work.completionTime }</p>
+                        <p>{ work.description }</p>
+                        <p><IoBookmark /> { work.note } </p>
+                    </div>
+                    <div className="photosContainer">
+                        <img src={imagesUrls[0].url} alt={"Image"} className="mainImage"/>
+                        <p>qui mettere foto piccole e selezionabili  (tipo amazon)</p>
+                    </div>
+                </div>
 
-                </ul>
+                <div className="materialsContainer">
+                    <h2>Materiali utilizzati</h2>
+                    <table>
+                        <thead>
+                        <tr>
+                            <th>Nome</th>
+                            <th>Unità</th>
+                            <th>Quantità</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {work.materials.map((material) => (
+                            <tr key={material.id}>
+                                <td>{material.name}</td>
+                                <td>{material.unit}</td>
+                                <td>{material.quantity}</td>
+                            </tr>
+                        ))}
+                        </tbody>
+                    </table>
+                </div>
+
+                <div className={"materialsContainer"}>
+                    <h2>Manodopera</h2>
+                    <table>
+                        <thead>
+                        <tr>
+                            <th>Data</th>
+                            <th>Lavoratore</th>
+                            <th>Ore</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {work.labour.map((labour) => (
+                            <tr >
+                                <td>{labour.date}</td>
+                                <td>{labour.users.map((user) =>
+                                    <p>{user.name} {user.surname} ha lavorato per {user.hours} ore e {user.minutes} minuti</p>
+                                )}</td>
+                            </tr>
+                        ))}
+                        </tbody>
+                    </table>
+                </div>
             </div>
     );
 }

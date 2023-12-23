fff3cb1483bb0239d729193f5858761ea556707b
mattiarainieri <mattiarainieri@live.it>
2023-12-23 09:53:55 +0100

    Allow to use comma instead dot in material quantity

--- src/functions/inputFieldChecker.js
+++ src/functions/inputFieldChecker.js
@@ -13,6 +13,7 @@ const inputFieldChecker = (inputs) => {
     validatedMaterials.pop()
 
     validatedMaterials.forEach((material) => {
+        material.quantity = material.quantity.replace(",", ".")
         material.quantity = parseInt(material.quantity)
         if (material.quantity <= 0 || isNaN(material.quantity) ) {
             throw new Error("materiale con quantità non valida")
